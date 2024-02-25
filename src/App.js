9a14bbcef77ba509364d8cb4167ef59d11b89e9a
Jahnavi <barmasaidurgajahnavi@gmail.com>
2024-02-25 05:41:28 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Userprofile from './Components/UserProfile/index'
+
+const userDetails = {
+  Image: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+  name: 'Rahul',
+  role: 'Software Engineer',
+}
+const App = () => <Userprofile />
+
+export default App
